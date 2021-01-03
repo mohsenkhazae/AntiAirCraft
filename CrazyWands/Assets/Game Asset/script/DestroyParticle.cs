8c28bb1bdc6903cc51d12e2851fea29c19f496bd
khazaee_home <mohsenkhazae@gmail.com>
2021-01-04 00:41:27 +0330

    add DestroyParticle.cs

---
+++ CrazyWands/Assets/Game Asset/script/DestroyParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour
+{
+    public float delayTime=6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(destroy());
+    }
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(delayTime);
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
